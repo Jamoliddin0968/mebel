6d68bd8301c34eb4e1d72eb00b67889ae97dd54d
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-12-11 08:21:30 +0500

    changrd all

--- config/urls.py
+++ config/urls.py
@@ -26,3 +26,4 @@ urlpatterns = [
 ]+drf_yasg_urlpatterns
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
