f0896b118e482628826788ca3a11253f1ff33d3b
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-12-15 17:36:14 +0500

    add receive

--- apps/receives/serializers.py
+++ apps/receives/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 from apps.receives.models import Receive
+from apps.warehouses.models import WareHouse
 
 
 class ReceiveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Receive
         fields = "__all__"
+
+    def create(self, validated_data):
+        obj = super().create(validated_data)
+        amount = obj.amount
+        product = obj.product
+        branch = obj.branch
+        warehouse_item, _ = WareHouse.objects.get_or_create(
+            product=product, branch=branch)
+        warehouse_item.amount += amount
+        warehouse_item.save()
+        return obj
