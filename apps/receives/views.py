bd631cfbab4186834a750aba043a0d0cb5576b1e
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-12-27 21:31:43 +0500

    sd

--- apps/receives/views.py
+++ apps/receives/views.py
@@ -1,20 +1,57 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
 from .serializers import ReceiveItemSerializer, ReceiveSerializer
 from .models import Receive, ReceiveItem
 from rest_framework.permissions import IsAuthenticated
 
+from drf_spectacular.utils import extend_schema
+
 
 class ReceiveViewSet(viewsets.ModelViewSet):
     queryset = Receive.objects.all()
     serializer_class = ReceiveSerializer
-
     permission_classes = [IsAuthenticated,]
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    @extend_schema(
+        tags=['Receive'],
+        description='Retrieve all receives'
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Receive'],
+        description='Create a new receive'
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Receive'],
+        description='Retrieve a specific receive by ID'
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Receive'],
+        description='Update a specific receive by ID'
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Receive'],
+        description='Partial update of a specific receive by ID'
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Receive'],
+        description='Delete a specific receive by ID'
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
 
 
 class ReceiveItemViewSet(viewsets.ModelViewSet):
@@ -22,3 +59,17 @@ class ReceiveItemViewSet(viewsets.ModelViewSet):
     serializer_class = ReceiveItemSerializer
     permission_classes = [IsAuthenticated,]
     http_method_names = ["delete", "patch"]
+
+    @extend_schema(
+        tags=['Receive Item'],
+        description='Delete a specific receive item by ID'
+    )
+    def destroy(self, request, *args, **kwargs):
+        return super().destroy(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Receive Item'],
+        description='Partially update a specific receive item by ID'
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
