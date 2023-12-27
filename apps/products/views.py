be654f00e70c1edba2b626e347036dd4aef7c263
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2023-12-27 21:07:45 +0500

    progress product

--- apps/products/views.py
+++ apps/products/views.py
@@ -6,11 +6,48 @@ from rest_framework.generics import CreateAPIView, DestroyAPIView
 from rest_framework import status
 from rest_framework.response import Response
 
+from drf_spectacular.utils import extend_schema
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.select_related('product_images').all()
     serializer_class = ProductSerializer
     http_method_names = ['get', 'post', 'put', 'patch']
+
+    @extend_schema(
+        tags=['Product'],
+        description='Retrieve all products'
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Product'],
+        description='Create a new product'
+    )
+    def create(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Product'],
+        description='Retrieve a specific product by ID'
+    )
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Product'],
+        description='Update a specific product by ID'
+    )
+    def update(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        tags=['Product'],
+        description='Partially update a specific product by ID'
+    )
+    def partial_update(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
 
 
 class ProductImageCreateAPIView(CreateAPIView):
