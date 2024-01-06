d456bf490896e403fa11017d94c14be6024b1441
Jamoliddin Ibrohimov <jamoliddin0968@gmail.com>
2024-01-06 23:19:34 +0500

    sd

--- apps/notifications/tasks.py
+++ apps/notifications/tasks.py
@@ -2,10 +2,15 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from apps.notifications.models import ScheduledNotification, Notification
 
+from apps.users.models import User
+
 
 @shared_task
 def create_notifications(*args, **kwargs):
+    users = User.objects.all()
     for notif in ScheduledNotification.objects.all():
-        # Notification.objects.create()
-        pass
+        for user in users:
+            Notification.objects.create(
+                message=notif.message, user=user
+            )
     return True
