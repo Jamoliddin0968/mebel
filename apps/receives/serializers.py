a993c9f8af1ab1f1df064be91e1f768aa5e24bf7
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-01-30 13:58:27 +0500

    df

--- apps/receives/serializers.py
+++ apps/receives/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Prefetch
 from rest_framework import serializers
 from apps.receives.models import Receive, ReceiveItem
 from apps.warehouse_items.models import WareHouseItem
@@ -7,15 +8,15 @@ from apps.warehouses.models import WareHouse
 class ReceiveItemSerializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = "__all__"
+        exclude = ("receive",)
         model = ReceiveItem
 
-        extra_kwargs = {
-            'receive': {'read_only': True}
-        }
+        # extra_kwargs = {
+        #     'receive': {'read_only': True}
+        # }
 
 class ReceiveSerializer(serializers.ModelSerializer):
-    receive_items = ReceiveItemSerializer(many=True)
+    receive_items = ReceiveItemSerializer(many=True,write_only=True)
 
     class Meta:
         model = Receive
@@ -28,19 +29,21 @@ class ReceiveSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # print(validated_data)
         items = validated_data.pop('receive_items')
-
-        obj = super().create(validated_data)
+        obj =  super(ReceiveSerializer, self).create(validated_data)
         branch = obj.branch
-        warehouse_item, _ = WareHouse.objects.get_or_create( branch=branch)
+        warehouse_item, _ = WareHouse.objects.get_or_create(branch=branch)
         receive_item_list = []
 
 
         for item in items:
             receive_item = ReceiveItem(**item)
             receive_item.receive = obj
-            warehouse_item,_ = WareHouseItem.objects.get_or_create(product_id=item.get('product_id'))
+            warehouse_item,_ = WareHouseItem.objects.get_or_create(product_id=item.get('product'))
             warehouse_item.amount += item.get('amount')
             warehouse_item.save()
             receive_item_list.append(receive_item)
         ReceiveItem.objects.bulk_create(receive_item_list)
+        # obj=Receive.objects.prefetch_related(
+        #         Prefetch('receive_items', queryset=ReceiveItem.objects.all())
+        #     ).get(id=obj.id)
         return obj
