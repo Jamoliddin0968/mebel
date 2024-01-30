411b17e6d09a84f8348aead8c2b8bbfbad1c59a7
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-01-30 20:12:20 +0500

    as

--- apps/receives/admin.py
+++ apps/receives/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Receive,ReceiveItem
+
+admin.site.register([Receive,ReceiveItem])
--- apps/sales/admin.py
+++ apps/sales/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Sale,SaleItem
+
+admin.site.register([Sale,SaleItem])
