e64bd52d133d4fd263a8d8c65b2f29a1bd48080f
jamoliddin0968 <jamoliddin0968@gmail.com>
2024-02-05 14:59:22 +0500

    ds

--- apps/receives/serializers.py
+++ apps/receives/serializers.py
@@ -30,14 +30,14 @@ class ReceiveSerializer(serializers.ModelSerializer):
         items = validated_data.pop('receive_items')
         obj =  super(ReceiveSerializer, self).create(validated_data)
         branch = obj.branch
-        warehouse_item, _ = WareHouse.objects.get_or_create(branch=branch)
+        warehouse, _ = WareHouse.objects.get_or_create(branch=branch)
         receive_item_list = []
 
 
         for item in items:
             receive_item = ReceiveItem(**item)
             receive_item.receive = obj
-            warehouse_item,_ = WareHouseItem.objects.get_or_create(product_id=item.get('product'))
+            warehouse_item,_ = WareHouseItem.objects.get_or_create(product_id=item.get('product'),warehouse=warehouse)
             warehouse_item.amount += item.get('amount')
             warehouse_item.save()
             receive_item_list.append(receive_item)
